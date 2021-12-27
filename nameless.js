7123b7aa761f22400e69f4f36fa44fcc471e7fab
darquesseval <74237632+darquesseval@users.noreply.github.com>
2021-12-27 11:10:23 +0100

    delete unnecessary stuff

--- nameless.js
+++
@@ -1,4 +0,0 @@
-var fs = require('fs');
-var imagedata // get imagedata from POST request
-fs.writeFile("/2dtodepth/infile/image.jpg", imagedata, 'binary', function(err) {
-    console.log("The file was saved!");
